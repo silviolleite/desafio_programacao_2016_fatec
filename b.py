013005fc240a4f720bcbec56e8700766e61cee77
Silvio Luis <silvio@orions.com.br>
2016-11-05 15:32:03 -0200

    atualizado

--- b.py
+++ b.py
@@ -1,11 +1,10 @@
 a = int(input())
 
 def res(texto, meio):
-    texto = texto[::-1]
     print("{}{}".format(texto[meio:], texto[:meio]))
    
 for i in range(a):
     texto = input()
     meio = int(len(texto)/2)
-    res(texto, meio)
+    res(texto[::-1], meio)
     
